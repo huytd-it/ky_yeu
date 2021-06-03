71d4014e805601022958ae506e02e2b3037f87da
tdhuy-it <yukihuy9999@gmail.com>
2021-06-03 18:14:23 +0700

    fix css

--- samples/ky_yeu/index.php
+++ samples/ky_yeu/index.php
@@ -29,6 +29,20 @@
             top: -300px !important;
             left: -475px !important;
         }
+        .btn {
+           
+            transition: .5 background-color;
+        }
+        .btn-left {
+            border-radius: 15px 0px 0px 15px ;
+        }
+        .btn-right {
+            border-radius: 0px 15px 15px 0px ;
+        }
+        .btn:hover {
+            background-color: black !important;
+           
+        }
     </style>
 </head>
 
@@ -49,9 +63,7 @@
         <div class="container">
             <div class="magazine">
                 <!-- Next button -->
-                <div ignore="1" class="next-button"></div>
-                <!-- Previous button -->
-                <div ignore="1" class="previous-button"></div>
+           
             </div>
         </div>
 
@@ -59,7 +71,22 @@
     <div class="flipbook-viewport">
         <div class="container">
             <div class="flipbook">
-
+                <!-- <div ignore="1" class="previous-button-2" style=" background:url(pics/arrows.png) no-repeat left center; background-color: #5c5e5c5c;
+                position: absolute;top:50%; transform:translate(-100%,-50%);
+    width: 40px;
+    height: 100%;
+    z-index: 600;"></div> -->
+                <div ignore="1" class="previous-button btn btn-left" style=" background:url(pics/arrows.png) no-repeat left center; background-color: #5c5e5c5c;
+                position: absolute;top:50%; transform:translate(-100%,-50%);
+    width: 40px;
+    height: 100%;
+    z-index: 600;"></div>
+                <!-- Previous button -->
+                <div ignore="1" class="next-button btn btn-right" style="  background:url(pics/arrows.png) no-repeat right center;  background-color: #5c5e5c5c;
+                position: absolute;
+    width: 40px;
+    height: 100%;top:50%; transform:translate(100%,-50%);
+    z-index: 600; right:0"></div>
                 <?php
 
                 foreach ($files1 as $key => $file) {
@@ -96,107 +123,139 @@
             }
             $('.flipbook .double').scissor();
             $('.flipbook').turn({
-                    // Elevation
-                    width: 960,
-                    height: 600,
-                    elevation: 50,
-                    gradients: true,
-                    duration: 1000,
-                    autoCenter: true,
-                    when: {
-                        turning: function(event, page, view) {
+                // Elevation
+                width: 960,
+                height: 600,
+                elevation: 50,
+                gradients: true,
+                duration: 1000,
+                autoCenter: true,
+                when: {
+                    turning: function(event, page, view) {
 
-                            var book = $(this),
-                                currentPage = book.turn('page'),
-                                pages = book.turn('pages');
+                        var book = $(this),
+                            currentPage = book.turn('page'),
+                            pages = book.turn('pages');
 
-                            // Update the current URI
+                        // Update the current URI
 
-                            Hash.go('page/' + page).update();
+                        Hash.go('page/' + page).update();
 
-                            // Show and hide navigation buttons
+                        // Show and hide navigation buttons
 
-                            disableControls(page);
-
-
-                            $('.thumbnails .page-' + currentPage).
-                            parent().
-                            removeClass('current');
-
-                            $('.thumbnails .page-' + page).
-                            parent().
-                            addClass('current');
+                        disableControls(page);
 
 
+                        $('.thumbnails .page-' + currentPage).
+                        parent().
+                        removeClass('current');
 
-                        },
-                    }
+                        $('.thumbnails .page-' + page).
+                        parent().
+                        addClass('current');
 
-                    }); Hash.on('^page\/([0-9]*)$', {
-                    yep: function(path, parts) {
-                        var page = parts[1];
 
-                        if (page !== undefined) {
-                            if ($('.flipbook').turn('is'))
-                                $('.flipbook').turn('page', page);
-                        }
 
                     },
-                    nop: function(path) {
+                }
 
+            });
+            Hash.on('^page\/([0-9]*)$', {
+                yep: function(path, parts) {
+                    var page = parts[1];
+
+                    if (page !== undefined) {
                         if ($('.flipbook').turn('is'))
-                            $('.flipbook').turn('page', 1);
+                            $('.flipbook').turn('page', page);
                     }
-                });
-                // Load the HTML4 version if there's not CSS transform
-                $("#slider").slider({
-                    min: 1,
-                    max: numberOfViews(flipbook),
 
-                    start: function(event, ui) {
+                },
+                nop: function(path) {
 
-                        if (!window._thumbPreview) {
-                            _thumbPreview = $('<div />', {
-                                'class': 'thumbnail'
-                            }).html('<div></div>');
-                            setPreview(ui.value);
-                            _thumbPreview.appendTo($(ui.handle));
-                        } else
-                            setPreview(ui.value);
+                    if ($('.flipbook').turn('is'))
+                        $('.flipbook').turn('page', 1);
+                }
+            });
+            // Load the HTML4 version if there's not CSS transform
+            $("#slider").slider({
+                min: 1,
+                max: numberOfViews(flipbook),
 
-                        moveBar(false);
+                start: function(event, ui) {
 
-                    },
-
-                    slide: function(event, ui) {
-
+                    if (!window._thumbPreview) {
+                        _thumbPreview = $('<div />', {
+                            'class': 'thumbnail'
+                        }).html('<div></div>');
+                        setPreview(ui.value);
+                        _thumbPreview.appendTo($(ui.handle));
+                    } else
                         setPreview(ui.value);
 
-                    },
+                    moveBar(false);
 
-                    stop: function() {
+                },
 
-                        if (window._thumbPreview)
-                            _thumbPreview.removeClass('show');
+                slide: function(event, ui) {
 
-                        $('.flipbook').turn('page', Math.max(1, $(this).slider('value') * 2 - 2));
+                    setPreview(ui.value);
 
-                    }
-                });
+                },
 
+                stop: function() {
 
-            }
-            // if ($.isTouch)
-            //     $('.flipbook-viewport').bind('zoom.doubleTap', zoomTo);
-            // else
-            //     $('.flipbook-viewport').bind('zoom.tap', zoomTo);
+                    if (window._thumbPreview)
+                        _thumbPreview.removeClass('show');
 
-            // if ($.isTouch) {
-            //     $('.flipbook').bind('touchstart', regionClick);
-            // } else {
-            //     $('.flipbook').click(regionClick);
-            // }
+                    $('.flipbook').turn('page', Math.max(1, $(this).slider('value') * 2 - 2));
 
+                }
+            });
+            $('.next-button').bind($.mouseEvents.over, function() {
+
+                $(this).addClass('next-button-hover');
+
+            }).bind($.mouseEvents.out, function() {
+
+                $(this).removeClass('next-button-hover');
+
+            }).bind($.mouseEvents.down, function() {
+
+                $(this).addClass('next-button-down');
+
+            }).bind($.mouseEvents.up, function() {
+
+                $(this).removeClass('next-button-down');
+
+            }).click(function() {
+
+                $('.flipbook').turn('next');
+
+            });
+
+            // Events for the next button
+
+            $('.previous-button').bind($.mouseEvents.over, function() {
+
+                $(this).addClass('previous-button-hover');
+
+            }).bind($.mouseEvents.out, function() {
+
+                $(this).removeClass('previous-button-hover');
+
+            }).bind($.mouseEvents.down, function() {
+
+                $(this).addClass('previous-button-down');
+
+            }).bind($.mouseEvents.up, function() {
+
+                $(this).removeClass('previous-button-down');
+
+            }).click(function() {
+
+                $('.flipbook').turn('previous');
+
+            });
             $(document).keydown(function(e) {
 
                 var previous = 37,
@@ -227,38 +286,53 @@
                 }
             });
 
-            $('.zoom-icon').bind('mouseover', function() {
 
-                if ($(this).hasClass('zoom-icon-in'))
-                    $(this).addClass('zoom-icon-in-hover');
+        }
+        // if ($.isTouch)
+        //     $('.flipbook-viewport').bind('zoom.doubleTap', zoomTo);
+        // else
+        //     $('.flipbook-viewport').bind('zoom.tap', zoomTo);
 
-                if ($(this).hasClass('zoom-icon-out'))
-                    $(this).addClass('zoom-icon-out-hover');
+        // if ($.isTouch) {
+        //     $('.flipbook').bind('touchstart', regionClick);
+        // } else {
+        //     $('.flipbook').click(regionClick);
+        // }
 
-            }).bind('mouseout', function() {
 
-                if ($(this).hasClass('zoom-icon-in'))
-                    $(this).removeClass('zoom-icon-in-hover');
 
-                if ($(this).hasClass('zoom-icon-out'))
-                    $(this).removeClass('zoom-icon-out-hover');
+        $('.zoom-icon').bind('mouseover', function() {
 
-            }).bind('click', function() {
+            if ($(this).hasClass('zoom-icon-in'))
+                $(this).addClass('zoom-icon-in-hover');
 
-                if ($(this).hasClass('zoom-icon-in'))
-                    $('.magazine-viewport').zoom('zoomIn');
-                else if ($(this).hasClass('zoom-icon-out'))
-                    $('.magazine-viewport').zoom('zoomOut');
+            if ($(this).hasClass('zoom-icon-out'))
+                $(this).addClass('zoom-icon-out-hover');
 
-            });
+        }).bind('mouseout', function() {
 
-            yepnope({
-                test: Modernizr.csstransforms,
-                yep: ['../../lib/turn.min.js'],
-                nope: ['../../lib/turn.html4.min.js', 'css/jquery.ui.html4.css'],
-                both: ['../../lib/zoom.min.js', '../../lib/scissor.min.js', 'css/double-page.css', 'css/jquery.ui.css', 'js/magazine.js', 'css/magazine.css'],
-                complete: loadApp
-            });
+            if ($(this).hasClass('zoom-icon-in'))
+                $(this).removeClass('zoom-icon-in-hover');
+
+            if ($(this).hasClass('zoom-icon-out'))
+                $(this).removeClass('zoom-icon-out-hover');
+
+        }).bind('click', function() {
+
+            if ($(this).hasClass('zoom-icon-in'))
+                $('.magazine-viewport').zoom('zoomIn');
+            else if ($(this).hasClass('zoom-icon-out'))
+                $('.magazine-viewport').zoom('zoomOut');
+
+        });
+
+        yepnope({
+            test: Modernizr.csstransforms,
+            yep: ['../../lib/turn.min.js'],
+            nope: ['../../lib/turn.html4.min.js', 'css/jquery.ui.html4.css'],
+            both: ['../../lib/zoom.min.js', '../../lib/scissor.min.js', 'css/double-page.css', 'css/jquery.ui.css', 'js/magazine.js', 'css/magazine.css'],
+            complete: loadApp
+        });
     </script>
 
 </body>
